8dd08b456b988123bfc7eeab3dd72dcce1f538b0
Sotiris Yiallourides <yiallouridess@cardiff.ac.uk>
2021-11-01 12:00:57 +0000

    Initial commit

---
+++ src/test/java/com/example/recruitmentsystemproject/RecruitmentSystemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recruitmentsystemproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentSystemProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
