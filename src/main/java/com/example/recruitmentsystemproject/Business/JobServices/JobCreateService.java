e2677489cb3d8dd36f62ac3f1fa100b5bd917dbd
c1847017 <yiallouridess@cardiff.ac.uk>
2021-11-03 22:15:43 +0000

    Add and implement Job, JobRepoJPA, JobRepo, JobCreateService, JobCreateServiceImpl, JobReadService, JobReadServiceImpl

---
+++ src/main/java/com/example/recruitmentsystemproject/Business/JobServices/JobCreateService.java
@@ -0,0 +1,8 @@
+package com.example.recruitmentsystemproject.Business.JobServices;
+
+import com.example.recruitmentsystemproject.Model.Job;
+
+public interface JobCreateService {
+
+    void saveJob(Job job);
+}
---
+++ src/main/java/com/example/recruitmentsystemproject/Business/JobServices/JobReadServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.recruitmentsystemproject.Business.JobServices;
+
+import com.example.recruitmentsystemproject.Model.Employer;
+import com.example.recruitmentsystemproject.Model.Job;
+import com.example.recruitmentsystemproject.Persistence.JobRepositories.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JobReadServiceImpl implements JobReadService{
+
+    @Autowired
+    private final JobRepo jobRepo;
+
+    public JobReadServiceImpl(JobRepo jobRepo) {
+        this.jobRepo = jobRepo;
+    }
+
+    @Override
+    public Optional<Job> findById(Long id) {
+        return jobRepo.findById(id);
+    }
+
+    @Override
+    public List<Job> findByEmployer(Employer employer) {
+        return jobRepo.findByEmployer(employer);
+    }
+
+    @Override
+    public List<Job> findByStatus(String status) {
+        return jobRepo.findByStatus(status);
+    }
+}
