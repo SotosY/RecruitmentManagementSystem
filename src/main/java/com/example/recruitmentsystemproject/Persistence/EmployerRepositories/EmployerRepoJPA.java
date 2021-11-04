270c201da37253737dbc4eeb2af636476161503c
c1847017 <yiallouridess@cardiff.ac.uk>
2021-11-04 15:38:13 +0000

    Add comments on RepoJPA classes

--- src/main/java/com/example/recruitmentsystemproject/Persistence/EmployerRepositories/EmployerRepoJPA.java
+++ src/main/java/com/example/recruitmentsystemproject/Persistence/EmployerRepositories/EmployerRepoJPA.java
@@ -8,7 +8,17 @@ import java.util.Optional;
 
 public interface EmployerRepoJPA extends JpaRepository<Employer, Long> {
 
+    /**
+     * Find an employer by id
+     * @param id
+     * @return employer
+     */
     Optional<Employer> findByEmployerId(Long id);
 
+    /**
+     * Find an employer by user
+     * @param user
+     * @return employer
+     */
     Optional<Employer> findByUser(User user);
 }
--- src/main/java/com/example/recruitmentsystemproject/Persistence/JobRepositories/JobRepoJPA.java
+++ src/main/java/com/example/recruitmentsystemproject/Persistence/JobRepositories/JobRepoJPA.java
@@ -9,10 +9,25 @@ import java.util.Optional;
 
 public interface JobRepoJPA extends JpaRepository<Job, Long> {
 
+    /**
+     * Find a job by id
+     * @param id
+     * @return job
+     */
     Optional<Job> findByJobId(Long id);
 
+    /**
+     * Get a list of jobs by employer
+     * @param employer
+     * @return list of jobs
+     */
     List<Job> findByEmployer(Employer employer);
 
+    /**
+     * Get a list of jobs by status
+     * @param status
+     * @return list of jobs
+     */
     List<Job> findByStatus(String status);
 
 }
