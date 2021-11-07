776ecf6d22a29f411e64f6e25e5e4c293ce723fd
c1847017 <yiallouridess@cardiff.ac.uk>
2021-11-07 22:31:33 +0000

    Update UserController

--- src/main/java/com/example/recruitmentsystemproject/Application/Controllers/UserController.java
+++ src/main/java/com/example/recruitmentsystemproject/Application/Controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.example.recruitmentsystemproject.Application.Controllers;
 
+import com.example.recruitmentsystemproject.Business.JobServices.JobReadService;
 import com.example.recruitmentsystemproject.Business.UserServices.UserReadService;
+import com.example.recruitmentsystemproject.Model.Job;
 import com.example.recruitmentsystemproject.Model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +17,10 @@ import java.util.List;
 public class UserController {
 
     @Autowired
-    private UserReadService userService;
+    private UserReadService userReadService;
+
+    @Autowired
+    private JobReadService jobReadService;
 
     @GetMapping("/login")
     public String login() {
@@ -27,8 +32,13 @@ public class UserController {
         return "register";
     }
 
-    @GetMapping("/employer")
+    @GetMapping("/users")
     public List<User> getAllUsers() {
-        return userService.findAll();
+        return userReadService.findAll();
+    }
+
+    @GetMapping
+    public  List<Job> getAllJobs() {
+        return jobReadService.findByStatus("Active");
     }
 }
