dafcea4d38e0aab7564c890491b02cff013ec489
c1847017 <yiallouridess@cardiff.ac.uk>
2021-11-09 20:37:02 +0000

    Update App.js

--- src/main/java/com/example/recruitmentsystemproject/Presentation/front_end/src/App.js
+++ src/main/java/com/example/recruitmentsystemproject/Presentation/front_end/src/App.js
@@ -1,15 +1,40 @@
-import './css/App.css';
+import './Components/css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import JobListComponent from "./Components/JobListComponent";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import LoginComponent from "./Components/LoginComponent";
+import ApplicantDashboard from "./Components/ApplicantComponents/ApplicantDashboard";
+import ApplicantProfile from "./Components/ApplicantComponents/ApplicantProfile";
+import ApplicantApplication from "./Components/ApplicantComponents/ApplicantApplication";
+import ApplicantJobInfo from "./Components/ApplicantComponents/ApplicantJobInfo";
+import ApplicantApplicationHistory from "./Components/ApplicantComponents/ApplicantApplicationHistory";
+import ApplicantRegister from "./Components/ApplicantComponents/ApplicantRegister";
+import EmployerRegister from "./Components/EmployerComponents/EmployerRegister";
+import NotFound from "./Components/NotFound";
 
 function App() {
   return (
     <div className="App">
-        <Header/>
-        <JobListComponent/>
-        <Footer />
+        <Router>
+            <Header />
+                <div>
+                    <Switch>
+                        <Route exact path = "/careers" component={JobListComponent}/>
+                        <Route exact path = "/careers/login" component={LoginComponent}/>
+                        <Route exact path = "/careers/register/a" component={ApplicantRegister} />
+                        <Route exact path = "/careers/register/e" component={EmployerRegister} />
+                        <Route exact path = "/careers/applicant/dashboard" component={ApplicantDashboard}/>
+                        <Route exact path = "/careers/applicant/profile" component={ApplicantProfile}/>
+                        <Route exact path = "/careers/applicant/application" component={ApplicantApplication}/>
+                        <Route exact path = "/careers/applicant/application-history" component={ApplicantApplicationHistory}/>
+                        <Route exact path = "/careers/applicant/application/job" component={ApplicantJobInfo}/>
+                        <Route path = "*" component={NotFound}/>
+                    </Switch>
+                </div>
+            <Footer />
+        </Router>
     </div>
   );
 }
