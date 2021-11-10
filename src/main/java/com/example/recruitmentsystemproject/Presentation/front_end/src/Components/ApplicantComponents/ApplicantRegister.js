6aa909ee168d929577f67713c633399572e87df0
c1847017 <yiallouridess@cardiff.ac.uk>
2021-11-10 22:04:57 +0000

    Registration now communicates with backend and database for ApplicantRegister.js and EmployerRegister.js

--- src/main/java/com/example/recruitmentsystemproject/Presentation/front_end/src/Components/ApplicantComponents/ApplicantRegister.js
+++ src/main/java/com/example/recruitmentsystemproject/Presentation/front_end/src/Components/ApplicantComponents/ApplicantRegister.js
@@ -1,15 +1,39 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import {Form} from "react-bootstrap";
 import '../css/Login-Register.css';
+import {Link, useHistory} from "react-router-dom";
+import {registerApplicant} from "../../Services/UserService";
 
-class ApplicantRegister extends Component {
-    render() {
+const ApplicantRegister = () => {
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useHistory();
+
+    const saveApplicantDetails = (e) => {
+        e.preventDefault();
+
+        const user = {email, password, firstName, lastName}
+        registerApplicant(user)
+            .then(res => {
+                console.log("Data added successfully", res.data);
+                history.push('/careers/applicant/dashboard')
+            })
+
+            .catch(onerror => {
+                console.log('Something went wrong', onerror);
+                history.push('/careers/applicant/dashboard')
+            });
+    }
+
         return (
             <div>
                 <main>
                     <section className='section-outside'>
                         <ol className="breadcrumb">
-                            <li><a href="http://localhost:3000/careers">CAREERS</a></li>
+                            <li><Link to="/careers">CAREERS</Link></li>
                             <li className='active'>REGISTER</li>
                         </ol>
                     </section>
@@ -17,32 +41,53 @@ class ApplicantRegister extends Component {
                     <Form className='container-login'>
 
                         <h1>Register</h1>
-                        <h2>Already have an account? <span className='h2_clickable'>Sign in</span></h2>
+                        <h2>Already have an account? <Link to='/careers/login' className='h2_clickable'>Sign in</Link></h2>
 
                         <div className='container-login-inside'>
 
                             <Form.Group className="mt-1" controlId="First name">
                                 <Form.Label>First name</Form.Label>
-                                <Form.Control type="text" placeholder="Enter your first name" />
+                                <Form.Control type="text"
+                                              placeholder="Enter your first name"
+                                              id='firstName'
+                                              value={firstName}
+                                              onChange={(e) => setFirstName(e.target.value)}
+                                />
                             </Form.Group>
 
                             <Form.Group className="mt-3" controlId="Last name">
                                 <Form.Label>Last name</Form.Label>
-                                <Form.Control type="text" placeholder="Enter your last name" />
+                                <Form.Control type="text"
+                                              placeholder="Enter your last name"
+                                              id='lastName'
+                                              value={lastName}
+                                              onChange={(e) => setLastName(e.target.value)}
+                                />
                             </Form.Group>
 
                             <Form.Group className="mt-3" controlId="formBasicEmail">
                                 <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" placeholder="Enter your email" />
+                                <Form.Control type="email"
+                                              placeholder="Enter your email"
+                                              id='email'
+                                              value={email}
+                                              onChange={(e) => setEmail(e.target.value)}
+
+                                />
                             </Form.Group>
 
                             <Form.Group className="mt-3" controlId="formBasicPassword">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control onChange='' type="password" placeholder="Enter your password" />
+                                <Form.Control type="password"
+                                              placeholder="Enter your password"
+                                              id='password'
+                                              value={password}
+                                              onChange={(e) => setPassword(e.target.value)}
+                                />
                             </Form.Group>
 
                             <center className='mt-5'>
-                                <button type="submit" className="btn btn-secondary" variant="primary">Sign Up</button>
+                                <button type="submit" className="btn btn-secondary" variant="primary" onClick={(e) => saveApplicantDetails(e)}>Sign Up</button>
                             </center>
 
                             <center className='mt-4'>
@@ -57,7 +102,6 @@ class ApplicantRegister extends Component {
                 </main>
             </div>
         );
-    }
 }
 
 export default ApplicantRegister;
--- src/main/java/com/example/recruitmentsystemproject/Presentation/front_end/src/Components/EmployerComponents/EmployerRegister.js
+++ src/main/java/com/example/recruitmentsystemproject/Presentation/front_end/src/Components/EmployerComponents/EmployerRegister.js
@@ -1,69 +1,121 @@
-import React, {Component} from 'react';
-import {DropdownButton, Form, Dropdown, InputGroup} from "react-bootstrap";
+import React, {useState} from 'react';
+import {Form} from "react-bootstrap";
 import '../css/Login-Register.css';
 import "tailwindcss/tailwind.css";
+import {Link, useHistory} from "react-router-dom";
+import UserService from "../../Services/UserService";
+import {registerEmployer} from "../../Services/UserService";
 
-class EmployerRegister extends Component {
-    render() {
-        return (
-            <div>
-                <main>
-                    <section className='section-outside'>
-                        <ol className="breadcrumb">
-                            <li><a href="http://localhost:3000/careers">CAREERS</a></li>
-                            <li className='active'>REGISTER</li>
-                        </ol>
-                    </section>
+const EmployerRegister = () => {
 
-                    <Form className='container-login'>
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [company, setCompany] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useHistory();
 
-                        <h1>Register</h1>
-                        <h2>Already have an account? <span className='h2_clickable'>Sign in</span></h2>
+    const saveEmployerDetails = (e) => {
+        e.preventDefault();
 
-                        <div className='container-login-inside'>
+        const user = {email, password, company, firstName, lastName}
+        registerEmployer(user)
+            .then(res => {
+                console.log("Data added successfully", res.data);
+                history.push('/careers/employer/dashboard')
+            })
 
-                            <Form.Group className="mt-1" controlId="First name">
-                                <Form.Label>First name</Form.Label>
-                                <Form.Control type="text" placeholder="Enter your first name" />
-                            </Form.Group>
-
-                            <Form.Group className="mt-3" controlId="Last name">
-                                <Form.Label>Last name</Form.Label>
-                                <Form.Control type="text" placeholder="Enter your last name" />
-                            </Form.Group>
-
-                            <Form.Group className="mt-3" controlId="Company">
-                                <Form.Label>Company</Form.Label>
-                                <Form.Control type="text" placeholder="Enter the company name" />
-                            </Form.Group>
-
-                            <Form.Group className="mt-3" controlId="formBasicEmail">
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" placeholder="Enter your email" />
-                            </Form.Group>
-
-                            <Form.Group className="mt-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control onChange='' type="password" placeholder="Enter your password" />
-                            </Form.Group>
-
-                            <center className='mt-5'>
-                                <button type="submit" className="btn btn-secondary" variant="primary">Sign Up</button>
-                            </center>
-
-                            <center className='mt-4'>
-                                <h2>By clicking the "Sign Up" button, you are creating
-                                    an account, and you agree to the <span className='h2_clickable'>Terms of use</span>.
-                                </h2>
-                            </center>
-
-                        </div>
-                    </Form>
-
-                </main>
-            </div>
-        );
+            .catch(onerror => {
+                console.log('Something went wrong', onerror);
+                history.push('/careers/employer/dashboard')
+            });
     }
+
+    return (
+        <div>
+            <main>
+                <section className='section-outside'>
+                    <ol className="breadcrumb">
+                        <li><Link to="/careers">CAREERS</Link></li>
+                        <li className='active'>REGISTER</li>
+                    </ol>
+                </section>
+
+                <Form className='container-login'>
+
+                    <h1>Register</h1>
+                    <h2>Already have an account? <Link to='/careers/login' className='h2_clickable'>Sign in</Link></h2>
+
+                    <div className='container-login-inside'>
+
+                        <Form.Group className="mt-1" controlId="First name">
+                            <Form.Label>First name</Form.Label>
+                            <Form.Control type="text"
+                                          placeholder="Enter your first name"
+                                          id='firstName'
+                                          value={firstName}
+                                          onChange={(e) => setFirstName(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group className="mt-3" controlId="Last name">
+                            <Form.Label>Last name</Form.Label>
+                            <Form.Control type="text"
+                                          placeholder="Enter your last name"
+                                          id='lastName'
+                                          value={lastName}
+                                          onChange={(e) => setLastName(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group className="mt-3" controlId="Company">
+                            <Form.Label>Company</Form.Label>
+                            <Form.Control type="text"
+                                          placeholder="Enter the company name"
+                                          id='company'
+                                          value={company}
+                                          onChange={(e) => setCompany(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group className="mt-3" controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email"
+                                          placeholder="Enter your email"
+                                          id='email'
+                                          value={email}
+                                          onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <Form.Group className="mt-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password"
+                                          placeholder="Enter your password"
+                                          id='password'
+                                          value={password}
+                                          onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </Form.Group>
+
+                        <center className='mt-5'>
+                            <button type="submit" className="btn btn-secondary" variant="primary" onClick={(e) => saveEmployerDetails(e)}>Sign Up</button>
+                        </center>
+
+                        <center className='mt-4'>
+                            <h2>
+                                By clicking the "Sign Up" button, you are
+                                creating an account, and you agree to the
+                                <span className='h2_clickable'>Terms of use</span>.
+                            </h2>
+                        </center>
+
+                    </div>
+                </Form>
+
+            </main>
+        </div>
+    );
 }
 
 export default EmployerRegister;
