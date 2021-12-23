b467de3ebe160fb02d6a1bb9a996c7c3e7add9a2
c1847017 <yiallouridess@cardiff.ac.uk>
2021-12-23 23:21:43 +0000

    Add FileStorageService, FileResponse, FileStorageProperties, FileNotFoundException and FileStorageException

---
+++ src/main/java/com/example/recruitmentsystemproject/Model/FileResponse.java
@@ -0,0 +1,17 @@
+package com.example.recruitmentsystemproject.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class FileResponse {
+
+    private String fileName;
+    private String fileDownloadUri;
+    private String fileType;
+    private Long fileSize;
+}
